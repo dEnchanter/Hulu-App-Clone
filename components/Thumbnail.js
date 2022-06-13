36cd72cd85adf827a394eef0bca4c3b9edb2feb8
dEnchanter <afolabiopaleye@gmail.com>
2022-06-13 12:14:53 +0100

    initial commit

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -2,8 +2,8 @@ import { ThumbUpIcon } from "@heroicons/react/outline"
 import Image from "next/image"
 import { forwardRef } from "react"
 
-export const Thumbnail = forwardRef(({ result }, ref) => {
-  const BASE_URL = "https://image.tmdb.org/t/p/w500"
+const Thumbnail = forwardRef(({ result }, ref) => {
+  const BASE_URL = "https://image.tmdb.org/t/p/w500";
 
   return (
     <div
@@ -25,10 +25,12 @@ export const Thumbnail = forwardRef(({ result }, ref) => {
         </h2>
         <p className="flex items-center opacity-0 group-hover:opacity-100">
           {result.media_type && `${result.media_type} *`}{" "}
-          {result.release_date || result.first_air_date} *{" "}
+          {result.release_date || result.first_air_date} {" "}
           <ThumbUpIcon className="h-5 mx-2" /> {result.vote_count}
         </p>
       </div>
     </div>
   )
 })
+
+export default Thumbnail;
